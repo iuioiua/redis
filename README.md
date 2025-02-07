a2ef16c3502d7df9ac27aa92a40b8e81880d1427
Asher Gomez <ashersaupingomez@gmail.com>
2025-02-07 16:47:14 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# redis
-Redis client for TypeScript
+# @iuioiua/redis
+
+[![JSR](https://jsr.io/badges/@iuioiua/redis)](https://jsr.io/@iuioiua/redis)
+[![CI](https://github.com/iuioiua/redis/actions/workflows/ci.yml/badge.svg)](https://github.com/iuioiua/redis/actions/workflows/ci.yml)
+[![codecov](https://codecov.io/gh/iuioiua/redis/branch/main/graph/badge.svg?token=8IDAVSL014)](https://r2d2-coverage.deno.dev/)
+
+Minimal [Redis](https://redis.io/) client for [Deno](https://deno.land/).
+
+```ts
+import { RedisClient } from "@iuioiua/redis";
+import { assertEquals } from "@std/assert/equals";
+
+using redisConn = await Deno.connect({ port: 6379 });
+const redisClient = new RedisClient(redisConn);
+
+const reply1 = await redisClient.sendCommand(["SET", "hello", "world"]);
+assertEquals(reply1, "OK");
+
+const reply2 = await redisClient.sendCommand(["GET", "hello"]);
+assertEquals(reply2, "world");
+```
+
+## Features
+
+- Supports RESPv2, RESP3, raw data, pipelining, pub/sub, transactions and Lua
+  scripts.
+- Compatible with timeouts and retries.
+- One of the fastest Redis clients in Deno.
+- Written to be easily understood and debugged.
+- Encourages the use of actual Redis commands without intermediate abstractions.
+
+## Resources
+
+- [Documentation](https://jsr.io/@iuioiua/redis/doc)
+- [Contributing guidelines](./CONTRIBUTING.md)
+
+## Size comparison
+
+| Module         | Size (KB) | Dependencies |
+| -------------- | --------- | ------------ |
+| @iuioiua/redis | 25.04     | 4            |
+| deno-redis     | 213.19    | 33           |
+| npm:ioredis    | 895.38    | 10           |
+| npm:redis      | 968.17    | 9            |
+
+Recorded on December 20, 2024.
+
+> Note: Results were produced using `deno info <module>`
---
+++ bench.ts
@@ -0,0 +1,95 @@
+import * as denoRedis from "@db/redis";
+import { Redis } from "ioredis";
+import { createClient } from "redis";
+
+import { RedisClient } from "./mod.ts";
+
+const hostname = "127.0.0.1";
+const port = 6379;
+
+const redisConn = await Deno.connect({ hostname, port });
+const redisClient = new RedisClient(redisConn);
+const denoRedisConn = await denoRedis.connect({ hostname, port });
+const ioRedis = new Redis();
+
+const nodeRedisClient = await createClient().connect();
+
+Deno.bench({
+  name: "@iuioiua/redis",
+  baseline: true,
+  async fn() {
+    await redisClient.sendCommand(["PING"]);
+
+    await redisClient.sendCommand(["SET", "mykey", "Hello"]);
+    await redisClient.sendCommand(["GET", "mykey"]);
+
+    await redisClient.sendCommand(["HSET", "hash", "a", "foo", "b", "bar"]);
+    await redisClient.sendCommand(["HGETALL", "hash"]);
+
+    await redisClient.pipelineCommands([
+      ["INCR", "X"],
+      ["INCR", "X"],
+      ["INCR", "X"],
+      ["INCR", "X"],
+    ]);
+  },
+});
+
+Deno.bench({
+  name: "deno-redis",
+  async fn() {
+    await denoRedisConn.ping();
+
+    await denoRedisConn.set("mykey", "Hello");
+    await denoRedisConn.get("mykey");
+
+    await denoRedisConn.hset("hash", { a: "foo", b: "bar" });
+    await denoRedisConn.hgetall("hash");
+
+    const pl = denoRedisConn.pipeline();
+    pl.incr("X");
+    pl.incr("X");
+    pl.incr("X");
+    pl.incr("X");
+    await pl.flush();
+  },
+});
+
+Deno.bench({
+  name: "npm:ioredis",
+  async fn() {
+    await ioRedis.ping();
+
+    await ioRedis.set("mykey", "Hello");
+    await ioRedis.get("mykey");
+
+    await ioRedis.hset("hash", { a: "foo", b: "bar" });
+    await ioRedis.hgetall("hash");
+
+    const pl = ioRedis.pipeline();
+    pl.incr("X");
+    pl.incr("X");
+    pl.incr("X");
+    pl.incr("X");
+    await pl.exec();
+  },
+});
+
+Deno.bench({
+  name: "npm:redis",
+  async fn() {
+    await nodeRedisClient.ping();
+
+    await nodeRedisClient.set("mykey", "Hello");
+    await nodeRedisClient.get("mykey");
+
+    await nodeRedisClient.hSet("hash", { a: "foo", b: "bar" });
+    await nodeRedisClient.hGetAll("hash");
+
+    // Auto-pipelining
+    await nodeRedisClient.incr("X");
+    await nodeRedisClient.incr("X");
+    await nodeRedisClient.incr("X");
+    await nodeRedisClient.incr("X");
+  },
+});
