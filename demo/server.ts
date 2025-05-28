ba58d3ef2acc6f9849f47af5fedd2c1d0d6eb06f
Asher Gomez <ashersaupingomez@gmail.com>
2025-05-29 08:55:20 +1000

    chore: minor refactor `html()` in demo server

--- demo/server.ts
+++ demo/server.ts
@@ -23,10 +23,12 @@ function html(
   strings: TemplateStringsArray,
   ...values: unknown[]
 ): string {
-  return strings.reduce(
-    (result, str, i) => result + str + (values[i] ?? ""),
-    "",
-  );
+  let out = "";
+  for (let i = 0; i < strings.length; ++i) {
+    out += strings[i];
+    if (i < values.length) out += values[i];
+  }
+  return out;
 }
 
 function renderHomePage(
